5d5614af27c161a8afa1076ca265aa306bb726be
Kunal Ahire <123294963+kunalahire1@users.noreply.github.com>
2024-03-09 10:45:04 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+  	public static void main(String[] args) {
+	B obj=new B();
+	obj.color();
+	}
+}
+class A{
+    public String color()
+    {
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+   @Override
+     public String color()
+    {
+        return "Invoking method from class A";
+    }
 }
