7c809ee660683b1b30f819783c91e3e0bd4bb7b5
Kunal Ahire <123294963+kunalahire1@users.noreply.github.com>
2024-03-10 18:21:12 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,26 @@
 package com.driver;
 
+
+
 public class Main {
-  	public static void main(String[] args) {
-	B obj=new B();
-	obj.color();
-	}
-}
-class A{
-    public String color()
-    {
+	public static class A {
+   public String meth() {
         return "Invoking method from class A";
     }
 }
-class B extends A{
-   @Override
-     public String color()
-    {
-        return "Invoking method from class A";
+
+public static class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
+    public static void main(String[] args) {
+    
+        B objB = new B();
+        System.out.println(objB.meth());
+
+       
+        System.out.println(objB.meth()); 
     }
 }
